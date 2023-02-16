a2aaae3fbeca9d13d0359663ffbe4e5f37ded630
nridd <nridd@NADINE>
2023-02-16 14:10:05 +0100

    Add project files.

---
+++ GitCheckpoint1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitCheckpoint1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Git checkpoint task 1.1");
+        }
+    }
+}
