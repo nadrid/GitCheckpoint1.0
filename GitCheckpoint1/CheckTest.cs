192c1e19865fdd40e42c58b35e47ce3cb865adb8
nridd <nridd@NADINE>
2023-02-16 14:48:28 +0100

    numbers

---
+++ GitCheckpoint1/CheckTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCheckpoint1
+{
+    public class CheckTest
+    {
+        public static void testMessage(string x)
+        {
+            x = "Task 2 checked";
+
+        }
+    }
+}
+ //Call this method from Main method in Program.cs and provide the text "Task 2 checked". Compile and run the program.
--- GitCheckpoint1/Program.cs
+++ GitCheckpoint1/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace GitCheckpoint1
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace GitCheckpoint1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("This is the Git checkpoint task 1.1");
+            
+            var CheckTest = new CheckTest();
+            CheckTest.testMessage("Task 2 checked");
+            Console.WriteLine();
+
+            var Calculate = new Calculate();
+            Calculate.CalculateName(5, 20);
+            
         }
+        
     }
 }
+//Call this method from Main method in Program.cs and provide the text "Task 2 checked". Compile and run the program.
